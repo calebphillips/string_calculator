da7640e24ab2db790dce206a641f04e2aa2b9711
calebphillips <caleb.phillips@gmail.com>
2011-03-29 14:43:56 -0500

    Updated to more 'functional' solution

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -1,32 +1,16 @@
 module StringCalculator
 
   def add
-    return 0 if empty?
     raise_if_negatives
-    digits.reduce {|sum, x| sum + x }
+    digits.reduce(0, :+)
   end
 
   def raise_if_negatives
+    negatives = digits.select {|x| x<0}
     raise "Negatives not allowed: #{negatives.join(", ")}" unless negatives.empty?
   end
 
-  def negatives
-    @negatives ||= digits.select {|x| x<0}
-  end
-
   def digits
-    gsub("\n",delimiter).split(delimiter).map{|x| x.to_i}
-  end
-
-  def delimiter
-    @delimiter ||= has_custom_delimiter? ? custom_delimiter : ','
-  end
-
-  def has_custom_delimiter?
-    self[0,2] == '//'
-  end
-
-  def custom_delimiter
-    self[2,1]
+    split(/[^\d|^-]/).reject(&:empty?).map(&:to_i)
   end
 end
